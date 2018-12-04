885baa906404849187509807963d797657481557
Kaige Yang <kaige.yang0110@gmail.com>
2018-12-04 08:16:24 +0000

    Add files via upload

--- test_models.py
+++ test_models.py
@@ -5,24 +5,27 @@ from sklearn.metrics.pairwise import rbf_kernel
 from sklearn.preprocessing import Normalizer
 from scipy.sparse import csgraph 
 import os 
-os.chdir('../code/')
+os.chdir('/home/kaige/Documents/code/')
+
 import datetime 
 import networkx as nx
 from utils import *
 
+timeRun = datetime.datetime.now().strftime('_%m_%d_%H_%M_%S') 
+newpath='/home/kaige/Documents/results/'
 
-user_num=100
-item_num=100
-dimension=20
+user_num=25
+item_num=200
+dimension=25
 noise_list=[0.01, 0.1, 0.25]
-loop_num=1
-
-_lambda=1
-g_lambda=1
+loop_num=3
+_lambda=0.1
+g_lambda=0.1
 iteration=item_num
 
 user_f,item_f,pos,ori_signal,adj,lap=generate_GMRF(user_num, item_num, dimension)
-
+# cov=np.cov(user_f)
+# lap=cov
 error_dict_ols={}
 error_dict_ridge={}
 error_dict_graph_ridge={}
@@ -32,23 +35,20 @@ for loop in range(loop_num):
 	for noise_scale in noise_list:
 		print('noise scale=%s'%(noise_scale))
 		noisy_signal=ori_signal+np.random.normal(size=(user_num, item_num), scale=noise_scale)
-
 		beta_ols=np.zeros((user_num, dimension))
 		beta_ridge=np.zeros((user_num, dimension))
 		beta_graph_ridge=np.zeros((user_num, dimension))
 		beta_gb=np.zeros((user_num, dimension))
-
 		error_list_ols=[]
 		error_list_ridge=[]
 		error_list_graph_ridge=[]
 		error_list_gb=[]
-
 		for i in range(iteration):
 			print('loop/total loop', loop, loop_num)
 			print('iteration i=', i )
 			print('noise scale=', noise_scale)
-			X=item_f[:,item_list[:i]].T
-			y=noisy_signal[:, item_list[:i]]
+			X=item_f[:,item_list[:i+1]].T
+			y=noisy_signal[:, item_list[:i+1]]
 			### OLS
 			beta_ols=ridge(X, y, 0.01, dimension)
 			error_ols=np.linalg.norm(beta_ols-user_f)
@@ -63,7 +63,7 @@ for loop in range(loop_num):
 			error_graph_ridge=np.linalg.norm(beta_graph_ridge-user_f)
 			error_list_graph_ridge.extend([error_graph_ridge])
 			# graph ridge iterative 
-			beta_gb=graph_ridge_iterative_model_no_mask(user_num, X, y, lap, beta_gb, dimension, g_lambda)
+			beta_gb=graph_ridge_iterative_model_no_mask(user_num, item_num, X, y, lap, beta_gb, dimension, g_lambda)
 			error_gb=np.linalg.norm(beta_gb-user_f)
 			error_list_gb.extend([error_gb])
 		if loop==0:
--- utils.py
+++ utils.py
@@ -6,19 +6,20 @@ from sklearn.preprocessing import Normalizer
 from scipy.sparse import csgraph 
 import datetime 
 import networkx as nx
+from sklearn.datasets.samples_generator import make_blobs
 
 
 def ols(x,y):
 	cov=np.dot(x.T,x)
 	temp2=np.linalg.inv(cov)
-	beta=np.dot(temp2, np.dot(x.T,y)).T
+	beta=np.dot(np.dot(y,x), temp2)
 	return beta 
 
 def ridge(x,y, _lambda, dimension):
 	cov=np.dot(x.T,x)
 	temp1=cov+_lambda*np.identity(dimension)
 	temp2=np.linalg.inv(temp1)
-	beta=np.dot(temp2, np.dot(x.T,y)).T
+	beta=np.dot(np.dot(y,x), temp2)
 	return beta 
 
 # Tikhonov
@@ -35,18 +36,7 @@ def graph_ridge(user_num, item_num, dimension, lap, item_f, mask, noisy_signal, 
 	sol=u.value 
 	return sol 
 
-def graph_ridge_no_mask(user_num, dimension, lap, item_f, noisy_signal, alpha):
-	u=cp.Variable((user_num, dimension))
-	l_signal=cp.matmul(u,item_f)
-	loss=cp.pnorm(noisy_signal-l_signal, p=2)**2
-	reg=cp.sum([cp.quad_form(u[:,d], lap) for d in range(dimension)])
-	#cons=[u<=1, u>=0]
-	alp=cp.Parameter(nonneg=True)
-	alp.value=alpha 
-	problem=cp.Problem(cp.Minimize(loss+alp*reg))
-	problem.solve()
-	sol=u.value 
-	return sol 
+
 
 def graph_ridge_iterative_model(item_f, noisy_signal, lap, beta_gb, dimension, user_list, item_list, user_dict, g_lambda):
 	user_nb=len(user_list)
@@ -81,25 +71,36 @@ def graph_ridge_iterative_model(item_f, noisy_signal, lap, beta_gb, dimension, u
 	return beta_gb
 
 
+def graph_ridge_no_mask(user_num, dimension, lap, item_f, noisy_signal, alpha):
+	u=cp.Variable((user_num, dimension))
+	l_signal=cp.matmul(u,item_f.T)
+	loss=cp.pnorm(noisy_signal-l_signal, p=2)**2
+	reg=cp.sum([cp.quad_form(u[:,d], lap) for d in range(dimension)])
+	#cons=[u<=1, u>=0]
+	alp=cp.Parameter(nonneg=True)
+	alp.value=alpha 
+	problem=cp.Problem(cp.Minimize(loss+alp*reg))
+	problem.solve()
+	sol=u.value 
+	return sol 
 
-
-def graph_ridge_iterative_model_no_mask(user_num, item_f, noisy_signal, lap, beta_gb, dimension, g_lambda):
+def graph_ridge_iterative_model_no_mask(user_num, item_num, item_f, noisy_signal, lap, beta_gb, dimension, g_lambda):
 	u=beta_gb.copy()
 	item_nb=len(item_f)
 	#update each atom
 	for k in range(dimension):
 		P=np.ones((user_num, item_nb))
-		P=P*(item_f[k]!=0)
+		P=P*(item_f.T[k]!=0)
 		p=P[0].reshape((1,item_nb))
 		_sum=np.zeros((user_num, item_nb))
 		for kk in range(dimension):
 			if kk==k:
 				pass 
 			else:
-				_sum+=np.dot(u[:,kk].reshape((user_num, 1)), item_f[kk,:].reshape((1, item_nb)))
+				_sum+=np.dot(u[:,kk].reshape((user_num, 1)), item_f.T[kk,:].reshape((1, item_nb)))
 		e=noisy_signal-_sum
 		ep=e*P
-		v_r=(item_f[k,:].reshape((1,item_nb))*p).T 
+		v_r=(item_f.T[k,:].reshape((1,item_nb))*p).T 
 		temp1=np.linalg.inv(np.dot(v_r.T, v_r)*np.identity(user_num)+g_lambda*lap)
 		temp2=np.dot(ep, v_r)
 		u[:,k]=np.dot(temp1, temp2).ravel()
@@ -179,10 +180,10 @@ def generate_graph_and_atom(user_num, item_num, dimension, noise_scale, p):
 	return U, item_f, noisy_signal, adj, lap 
 
 
-
-
 def generate_artificial_graph(user_num):
-	pos=np.random.uniform(size=(user_num, 2))
+	#pos=np.random.uniform(size=(user_num, 2))
+	pos, y = make_blobs(n_samples=user_num, centers=5, n_features=2, random_state=0)
+	pos=Normalizer().fit_transform(pos)
 	adj=rbf_kernel(pos)
 	np.fill_diagonal(adj,0)
 	lap=csgraph.laplacian(adj, normed=False)
