413c39fcd0e761abea5bc1153cd7debe4d657835
Kaige Yang <kaige.yang0110@gmail.com>
2019-01-12 17:28:54 +0000

    Add files via upload

--- bandit_models.py
+++ bandit_models.py
@@ -177,5 +177,111 @@ class Graph_ridge():
 
 
 
+class Graph_ridge_simple():
+	def __init__(self, user_num, item_num, dimension, lap, adj, pool_size, user_features, item_features, noisy_signal, alpha, confidence_inter):
+		self.lap=lap+np.identity(user_num)
+		self.adj=adj
+		self.mask=np.zeros((user_num, item_num))
+		self.dimension=dimension
+		self.user_num=user_num
+		self.item_num=item_num
+		self.item_features=item_features
+		self.user_features=user_features
+		self.es_user_f=np.zeros((user_num, dimension))
+		self.noisy_signal=noisy_signal
+		self.user_cov={}
+		self.bias={}
+		self.served_users=[]
+		self.served_items=[]
+		self.alpha=alpha
+		self.c_t=confidence_inter
+		self.user_mask=np.zeros((user_num, dimension))
+		self.item_mask=np.zeros((item_num, dimension))
+
+	def update_user_features(self, selected_user, picked_item, reward, time):
+		self.user_cov[selected_user]+=np.outer(self.item_features[picked_item], self.item_features[picked_item])+self.alpha*np.identity(self.dimension)
+		self.bias[selected_user]+=self.item_features[picked_item]*reward
+		#### update all user at each iteration
+		# u=cp.Variable((self.user_num, self.dimension))
+		# l_signal=cp.multiply(cp.matmul(u, self.item_features.T), self.mask)
+		# loss=cp.norm(cp.multiply(self.noisy_signal, self.mask)-l_signal, 'fro')**2
+		# reg=cp.sum([cp.quad_form(u[:,d], self.lap) for d in range(self.dimension)])
+		# alp=cp.Parameter(nonneg=True)
+		# alp.value=self.alpha 
+		# problem=cp.Problem(cp.Minimize(loss+alp*reg))
+		# problem.solve()
+		# self.es_user_f=u.value 
+		##### only update the user and its neighbors
+		if (time%20==0) or (time==0):
+			neighbors=list(np.where(self.adj[selected_user]>0)[0])
+			neighbor_num=len(neighbors)
+			print('neighbor_num', neighbor_num)
+			lap=self.lap[neighbors][:, neighbors]
+			item_f=self.item_features[self.served_items]
+			mask=self.mask[neighbors][:,self.served_items]
+			noisy_signal=self.noisy_signal[neighbors][:, self.served_items]
+			self.es_user_f[neighbors]=self.graph_ridge_solver(neighbor_num, mask, item_f, noisy_signal, lap)
+		else:
+			pass
+
+	def graph_ridge_solver(self, neighbor_num, mask, item_f, noisy_signal, lap):
+		u=cp.Variable((neighbor_num, self.dimension))
+		l_signal=cp.multiply(cp.matmul(u, item_f.T), mask)
+		loss=cp.norm(cp.multiply(noisy_signal, mask)-l_signal, 'fro')**2
+		reg=cp.sum([cp.quad_form(u[:,d], lap) for d in range(self.dimension)])
+		alp=cp.Parameter(nonneg=True)
+		alp.value=self.alpha 
+		problem=cp.Problem(cp.Minimize(loss+alp*reg))
+		problem.solve()	
+		solution=u.value
+		return solution
+
+
+	def choose_item(self, selected_user, item_pool):
+		a=self.item_features[item_pool]
+		mean=np.dot(a,self.es_user_f[selected_user])
+		b=np.linalg.pinv(self.user_cov[selected_user])
+		weighted_norm=np.diagonal(np.sqrt(np.dot(a, np.dot(b, a.T))))
+		sum_=mean+self.c_t*weighted_norm
+		index=np.argmax(sum_)
+		picked_item=item_pool[index]
+		reward=self.noisy_signal[selected_user, picked_item]
+		best_reward=np.max(self.noisy_signal[selected_user, item_pool])
+		regret=best_reward-reward
+		return picked_item, reward, regret
+
+	def run(self, random_user_list, random_item_pool, iteration):
+		cum_regret=[0]
+		error_list=[]
+		error_all_array=np.zeros((iteration, self.user_num))
+		trace_list=[]
+		for i in range(iteration):
+			print('iteration', i)
+			selected_user=random_user_list[i]
+			item_pool=random_item_pool[i]
+			if selected_user in self.served_users:
+				pass 
+			else:
+				self.served_users.extend([selected_user])
+				self.user_cov[selected_user]=np.zeros((self.dimension, self.dimension))
+				self.bias[selected_user]=np.zeros(self.dimension)
+			picked_item, reward, regret=self.choose_item(selected_user, item_pool)
+			if picked_item in self.served_items:
+				pass 
+			else:
+				self.served_items.extend([picked_item])
+			self.mask[selected_user, picked_item]=1
+			self.user_mask[selected_user,:]=1
+			self.item_mask[picked_item,:]=1
+			self.update_user_features(selected_user, picked_item, reward, i)
+			cum_regret.extend([cum_regret[-1]+regret])
+			error=np.linalg.norm(self.es_user_f-self.user_features, 'fro')
+			error_list.extend([error])
+			error_all_user=np.linalg.norm(self.es_user_f-self.user_features, axis=1)
+			error_all_array[i,:]=error_all_user
+		return cum_regret, error_list, error_all_array
+
+
+
 
 
--- graph_ridge_and_bandit.py
+++ graph_ridge_and_bandit.py
@@ -8,16 +8,16 @@ import os
 os.chdir('Documents/research/code/')
 import datetime 
 import networkx as nx
-from bandit_models import LinUCB, Graph_ridge
+from bandit_models import LinUCB, Graph_ridge, Graph_ridge_simple
 from sklearn import datasets
 from utils import *
 path='../results/LapUCB_results/'
 
-user_num=30
+user_num=20
 item_num=100
 dimension=10
-iteration=10000
-noise_level=0.5
+iteration=5000
+noise_level=0.1
 
 confidence_inter=0.05
 
@@ -33,6 +33,9 @@ thrs=np.round((min_adj+max_adj)/2, decimals=2)
 thrs=0
 adj[adj<=thrs]=0
 lap=csgraph.laplacian(adj, normed=False)
+lap_evalues, lap_evectors=np.linalg.eig(lap)
+lap_evalues=np.sort(lap_evalues)
+evalues_matrix=np.diag(lap_evalues)
 
 clear_signal=np.dot(user_f, item_f.T)
 noise=np.random.normal(size=(user_num, item_num), scale=noise_level)
@@ -42,8 +45,8 @@ pool_size=10
 user_array=np.random.choice(np.arange(user_num), size=iteration)
 item_array=np.random.choice(np.arange(item_num), size=iteration*pool_size).reshape((iteration, pool_size))
 
-lin_lam=0.1
-graph_lam=0.1
+lin_lam=0.01
+graph_lam=0.01
 
 linucb=LinUCB(user_num, item_num, dimension, pool_size, user_f, item_f,noisy_signal, lin_lam, confidence_inter)
 l_cum_regret, l_error_list, l_e_array=linucb.run(user_array, item_array, iteration)
@@ -51,25 +54,30 @@ l_cum_regret, l_error_list, l_e_array=linucb.run(user_array, item_array, iterati
 graph_ridge=Graph_ridge(user_num, item_num, dimension, lap, adj, pool_size, user_f, item_f, noisy_signal, graph_lam, confidence_inter)
 g_cum_regret, g_error_list, g_e_array=graph_ridge.run(user_array, item_array, iteration)
 
+graph_ridge_s=Graph_ridge_simple(user_num, item_num, dimension, evalues_matrix, adj, pool_size, user_f, item_f, noisy_signal, graph_lam, confidence_inter)
+g_cum_regret2, g_error_list2, g_e_array2=graph_ridge_s.run(user_array, item_array, iteration)
+
 
 plt.figure(figsize=(5,5))
 plt.plot(l_cum_regret, 'r', label='LinUCB')
 plt.plot(g_cum_regret, 'y', label='LapUCB')
+plt.plot(g_cum_regret2, 'g', label='LamUCB')
 plt.xlabel('Iteration', fontsize=12)
 plt.ylabel('Cum regret', fontsize=12)
 plt.title('User num=%s, noise=%s, T=%s'%(user_num, noise_level, thrs), fontsize=12)
 plt.legend(loc=0, fontsize=12)
-plt.savefig(path+'random_model/'+'cum_regret_user_num_noise_thrs_%s_%s_%s'%(user_num, noise_level, thrs)+'.png', dpi=300)
+plt.savefig(path+'random_model/'+'LinUCB_vs_LapUCB_cum_regret_user_num_noise_thrs_%s_%s_%s'%(user_num, noise_level, thrs)+'.png', dpi=300)
 plt.show()
 
 plt.figure(figsize=(5,5))
 plt.plot(l_error_list, 'r', label='LinUCB')
 plt.plot(g_error_list, 'y', label='LapUCB')
+plt.plot(g_error_list2, 'g',label='LamUCB')
 plt.xlabel('Iteration', fontsize=12)
 plt.ylabel('Learning Error', fontsize=12)
 plt.title('User num=%s, noise=%s, T=%s'%(user_num, noise_level, thrs), fontsize=12)
 plt.legend(loc=0, fontsize=12)
-plt.savefig(path+'random_model/'+'learing_error_user_num_noise_thrs_%s_%s_%s'%(user_num, noise_level, thrs)+'.png', dpi=300)
+plt.savefig(path+'random_model/'+'LinUCB_vs_LapUCB_learning_error_user_num_noise_thrs_%s_%s_%s'%(user_num, noise_level, thrs)+'.png', dpi=300)
 plt.show()
 
 
@@ -112,10 +120,10 @@ plt.show()
 fig, (ax1, ax2)=plt.subplots(1,2, figsize=(8,5))
 ax1.plot(l_e_array[:,:len(labels)+1])
 ax1.legend(loc=0, labels=labels, fontsize=10)
-ax1.set_ylim([0.0, 2.0])
+ax1.set_ylim([0.0, 1.2])
 ax1.set_title('LinUCB',fontsize=12)
 ax2.plot(g_e_array[:, :len(labels)+1])
-ax2.set_ylim([0.0, 2.0])
+ax2.set_ylim([0.0, 1.2])
 ax2.legend(loc=0, labels=labels, fontsize=10)
 ax2.set_title('LapUCB', fontsize=12)
 plt.xlabel('User num=%s, noise=%s'%(user_num, noise_level), fontsize=10)
